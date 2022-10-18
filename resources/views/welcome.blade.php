16bd372c04726af2604aa4bbfe889199d98641cc
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-10-18 13:01:38 +0200

    creating data base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="../css/style.css" rel="stylesheet">
+        <link href="{{ asset('style.css') }}" rel="stylesheet">
         <title>Laravel</title>
 
         <!-- Fonts -->
