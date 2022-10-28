b1fdcab4fec1c802bf4595d2571f663c44582ccb
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-10-28 09:23:06 +0200

    search promotion is fixed

--- public/js/script.js
+++ public/js/script.js
@@ -11,18 +11,8 @@ search_btn.addEventListener("keyup", function () {
             return response.text()
         })).then((data) => {
             console.log(data);
-            promotion_data.innerHTML = "";
-            for (var i = 0; i < data.length; i++) {
-                promotion_data.innerHTML += ` 
-                <tr>
-                    <td>${data[i]["name"]}</td>
-                    <td>
-                    <a href='"/edit_form/{{ $row->id }}">edit'>Edit</a>
-                    <a href='showPromo.php?id_delete=${data[i]["id"]}'> Delete</a>
-                    </td>
-                </tr>
-                ;`
-            }
+            promotion_data.innerHTML = data;
+
         })
 
 })
